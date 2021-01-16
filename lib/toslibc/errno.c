b2a4b03c58f9f5c830219df1171e72019b1bc3b8
Fredrik Noring <noring@nocrew.org>
2021-01-16 09:16:15 +0100

    Fix errno_for_tos_error and tos_error_message

--- lib/toslibc/errno.c
+++ lib/toslibc/errno.c
@@ -11,20 +11,20 @@ int errno;
 int errno_for_tos_error(int toserr)
 {
 	switch (toserr) {
-#define _TOS_ERROR_ERRNO(identifier, number, errno, description)	\
-	case -TOS_E##identifier: return -errno;
-	_TOS_ERRORS(_TOS_ERROR_ERRNO)
+#define _TOS_ERROR_ERRNO(identifier_, number_, errno_, description_)	\
+	case TOS_E##identifier_: return errno_;
+_TOS_ERRORS(_TOS_ERROR_ERRNO)
 	}
 
-	return -1000 < toserr && toserr < 0 ? -EINVAL : toserr;
+	return 0 < toserr && toserr < 1000 ? EINVAL : toserr;
 }
 
 const char *tos_error_message(int toserr)
 {
 	switch (toserr) {
-#define _TOS_ERROR_MSG(identifier, number, errno, description)		\
-	case TOS_E##identifier: return description;
-	_TOS_ERRORS(_TOS_ERROR_MSG)
+#define _TOS_ERROR_MSG(identifier_, number_, errno_, description_)	\
+	case TOS_E##identifier_: return description_;
+_TOS_ERRORS(_TOS_ERROR_MSG)
 	}
 
 	return "Unknown error";
