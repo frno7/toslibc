56acf4de4a06e7610a491f4cc8b49e2decd2f89d
Fredrik Noring <noring@nocrew.org>
2025-08-23 15:30:23 +0200

    Prefix PSG registers with SND_

--- include/toslibc/asm/snd/psg.h
+++ include/toslibc/asm/snd/psg.h
@@ -7,10 +7,10 @@
 #include <asm/machine.h>
 #include <asm/math.h>
 
-#define PSG_ADDR_RD_SELECT	0xff8800
-#define PSG_ADDR_WR		0xff8802
+#define SND_PSG_ADDR_RD_SELECT	0xff8800
+#define SND_PSG_ADDR_WR		0xff8802
 
-#define PSG_REGISTERS(reg)						\
+#define SND_PSG_REGISTERS(reg)						\
 	reg( 0, plo_a,   PLO_A,   "Period of channel A fine tone")	\
 	reg( 1, phi_a,   PHI_A,   "Period of channel A rough tone")	\
 	reg( 2, plo_b,   PLO_B,   "Period of channel B fine tone")	\
@@ -29,63 +29,63 @@
 	reg(15, io_b,    IO_B,    "Data of I/O port B")
 
 enum {
-#define PSG_REG_ENUM(register_, symbol_, label_, description_)		\
-	PSG_REG_##label_ = register_,
-PSG_REGISTERS(PSG_REG_ENUM)
+#define SND_PSG_REG_ENUM(register_, symbol_, label_, description_)	\
+	SND_PSG_REG_##label_ = register_,
+SND_PSG_REGISTERS(SND_PSG_REG_ENUM)
 };
 
 enum {
-	PSG_IOMIX_OFF     = 0b111111,
-	PSG_IOMIX_TONE_A  = 0b111110,
-	PSG_IOMIX_TONE_B  = 0b111101,
-	PSG_IOMIX_TONE_C  = 0b111011,
-	PSG_IOMIX_NOISE_A = 0b110111,
-	PSG_IOMIX_NOISE_B = 0b101111,
-	PSG_IOMIX_NOISE_C = 0b011111
+	SND_PSG_IOMIX_OFF     = 0b111111,
+	SND_PSG_IOMIX_TONE_A  = 0b111110,
+	SND_PSG_IOMIX_TONE_B  = 0b111101,
+	SND_PSG_IOMIX_TONE_C  = 0b111011,
+	SND_PSG_IOMIX_NOISE_A = 0b110111,
+	SND_PSG_IOMIX_NOISE_B = 0b101111,
+	SND_PSG_IOMIX_NOISE_C = 0b011111
 };
 
-static inline uint8_t psg_rd(uint8_t reg)
+static inline uint8_t snd_psg_rd(uint8_t reg)
 {
-	iowr8(reg, PSG_ADDR_RD_SELECT);
+	iowr8(reg, SND_PSG_ADDR_RD_SELECT);
 
-	return iord8(PSG_ADDR_RD_SELECT);
+	return iord8(SND_PSG_ADDR_RD_SELECT);
 }
 
-static inline void psg_wr(uint8_t value, uint8_t reg)
+static inline void snd_psg_wr(uint8_t value, uint8_t reg)
 {
-	iowr8(reg,   PSG_ADDR_RD_SELECT);
-	iowr8(value, PSG_ADDR_WR);
+	iowr8(reg,   SND_PSG_ADDR_RD_SELECT);
+	iowr8(value, SND_PSG_ADDR_WR);
 }
 
-static inline uint16_t psg_tone_period_freq(uint32_t freq)
+static inline uint16_t snd_psg_tone_period_freq(uint32_t freq)
 {
 	return DIV_ROUND_CLOSEST_U32(ATARI_STE_PSG_CLK, 16 * freq);
 }
 
-#define PSG_DEFINE_CHANNEL_F(ch, CH)					\
-static inline void psg_wr_period_##ch(uint16_t period)			\
+#define SND_PSG_DEFINE_CHANNEL_F(ch, CH)				\
+static inline void snd_psg_wr_period_##ch(uint16_t period)		\
 {									\
-	psg_wr(period & 0xff, PSG_REG_PLO_##CH);			\
-	psg_wr(period >> 8,   PSG_REG_PHI_##CH);			\
+	snd_psg_wr(period & 0xff, SND_PSG_REG_PLO_##CH);		\
+	snd_psg_wr(period >> 8,   SND_PSG_REG_PHI_##CH);		\
 }									\
 									\
-static inline void psg_wr_freq_##ch(uint16_t freq)			\
+static inline void snd_psg_wr_freq_##ch(uint16_t freq)			\
 {									\
-	psg_wr_period_##ch(psg_tone_period_freq(freq));			\
+	snd_psg_wr_period_##ch(snd_psg_tone_period_freq(freq));		\
 }									\
 									\
-static inline void psg_wr_level_##ch(uint8_t level)			\
+static inline void snd_psg_wr_level_##ch(uint8_t level)			\
 {									\
-	psg_wr(level, PSG_REG_LEVEL_##CH);				\
+	snd_psg_wr(level, SND_PSG_REG_LEVEL_##CH);			\
 }
 
-PSG_DEFINE_CHANNEL_F(a, A)
-PSG_DEFINE_CHANNEL_F(b, B)
-PSG_DEFINE_CHANNEL_F(c, C)
+SND_PSG_DEFINE_CHANNEL_F(a, A)
+SND_PSG_DEFINE_CHANNEL_F(b, B)
+SND_PSG_DEFINE_CHANNEL_F(c, C)
 
-static inline void psg_wr_iomix(uint8_t iomix)
+static inline void snd_psg_wr_iomix(uint8_t iomix)
 {
-	psg_wr(iomix, PSG_REG_IOMIX);
+	snd_psg_wr(iomix, SND_PSG_REG_IOMIX);
 }
 
 #endif /* TOSLIBC_ASM_SND_PSG_H */
