7731a554b812f99385fb837386b4db5499c038ff
Fredrik Noring <noring@nocrew.org>
2025-09-07 10:01:05 +0200

    Rename DMA sound registers similar to STE Developer Addendum document

--- include/toslibc/asm/snd/dma.h
+++ include/toslibc/asm/snd/dma.h
@@ -6,17 +6,17 @@
 #include <asm/io.h>
 
 #define SND_DMA_REGISTERS(reg)						\
-	reg( 0, ctrl,    CTRL,    "Sample control")			\
-	reg( 1, starthi, STARTHI, "Sample start address high byte")	\
-	reg( 2, startmi, STARTMI, "Sample start address middle byte")	\
-	reg( 3, startlo, STARTLO, "Sample start address low byte")	\
-	reg( 4, counthi, COUNTHI, "Sample count address high byte")	\
-	reg( 5, countmi, COUNTMI, "Sample count address middle byte")	\
-	reg( 6, countlo, COUNTLO, "Sample count address low byte")	\
-	reg( 7, endhi,   ENDHI,   "Sample start address high byte")	\
-	reg( 8, endmi,   ENDMI,   "Sample start address middle byte")	\
-	reg( 9, endlo,   ENDLO,   "Sample start address low byte")	\
-	reg(16, mode,    MODE,    "Sample mode")
+	reg( 0, ctrl,      CTRL,      "Sound DMA control")		\
+	reg( 1, basehi,	   BASEHI,    "Frame base address high byte")	\
+	reg( 2, basemi,	   BASEMI,    "Frame base address middle byte")	\
+	reg( 3, baselo,	   BASELO,    "Frame base address low byte")	\
+	reg( 4, counterhi, COUNTERHI, "Frame address counter high byte") \
+	reg( 5, countermi, COUNTERMI, "Frame address counter middle byte") \
+	reg( 6, counterlo, COUNTERLO, "Frame address counter low byte")	\
+	reg( 7, endhi,     ENDHI,     "Frame end address high byte")	\
+	reg( 8, endmi,     ENDMI,     "Frame end address middle byte")	\
+	reg( 9, endlo,     ENDLO,     "Frame end address low byte")	\
+	reg(16, mode,      MODE,      "Sound mode control")
 
 enum snd_dma_reg {
 #define SND_DMA_REG_ENUM(register, symbol, label, description)		\
@@ -39,35 +39,35 @@ struct snd_dma_mode {
 	union {
 		struct {
 			uint16_t : 8;
-			uint16_t mode : 2;
+			uint16_t format : 2;
 			uint16_t : 4;
-			uint16_t frequency : 2;
+			uint16_t rate : 2;
 		};
 		uint16_t u16;
 	};
 };
 
-#define SND_DMA_MODE(m)							\
+#define SND_DMA_MODE_FORMAT(m)						\
 	m(0, STEREO8)							\
 	m(1, STEREO16)							\
 	m(2, MONO8)
 
 enum {
-#define SND_DMA_MODE_ENUM(value, mode)					\
-	SND_DMA_MODE_##mode = value,
-SND_DMA_MODE(SND_DMA_MODE_ENUM)
+#define SND_DMA_MODE_FORMAT_ENUM(value, format)				\
+	SND_DMA_MODE_FORMAT_##format = value,
+SND_DMA_MODE_FORMAT(SND_DMA_MODE_FORMAT_ENUM)
 };
 
-#define SND_DMA_FREQUENCY(f)						\
-	f(0, 6258)							\
-	f(1, 12517)							\
-	f(2, 25033)							\
-	f(3, 50066)
+#define SND_DMA_MODE_RATE(r)						\
+	r(0, 6258)							\
+	r(1, 12517)							\
+	r(2, 25033)							\
+	r(3, 50066)
 
 enum {
-#define SND_DMA_FREQUENCY_ENUM(value, frequency)			\
-	SND_DMA_FREQUENCY_##frequency = value,
-SND_DMA_FREQUENCY(SND_DMA_FREQUENCY_ENUM)
+#define SND_DMA_MODE_FREQUENCY_ENUM(value, frequency)			\
+	SND_DMA_MODE_FREQUENCY_##frequency = value,
+SND_DMA_MODE_RATE(SND_DMA_MODE_FREQUENCY_ENUM)
 };
 
 #define SND_DMA_REG_RD16_WR16(symbol, label)				\
@@ -91,15 +91,15 @@ SND_DMA_REG_RD16_WR16(mode, MODE)
 	{								\
 		return iowr8(value, SND_DMA_ADDR_##label + 1);		\
 	}
-SND_DMA_REG_RD8_WR8(starthi, STARTHI)
-SND_DMA_REG_RD8_WR8(startmi, STARTMI)
-SND_DMA_REG_RD8_WR8(startlo, STARTLO)
-SND_DMA_REG_RD8_WR8(counthi, COUNTHI)
-SND_DMA_REG_RD8_WR8(countmi, COUNTMI)
-SND_DMA_REG_RD8_WR8(countlo, COUNTLO)
-SND_DMA_REG_RD8_WR8(endhi,   ENDHI)
-SND_DMA_REG_RD8_WR8(endmi,   ENDMI)
-SND_DMA_REG_RD8_WR8(endlo,   ENDLO)
+SND_DMA_REG_RD8_WR8(basehi,    BASEHI)
+SND_DMA_REG_RD8_WR8(basemi,    BASEMI)
+SND_DMA_REG_RD8_WR8(baselo,    BASELO)
+SND_DMA_REG_RD8_WR8(counterhi, COUNTERHI)
+SND_DMA_REG_RD8_WR8(countermi, COUNTERMI)
+SND_DMA_REG_RD8_WR8(counterlo, COUNTERLO)
+SND_DMA_REG_RD8_WR8(endhi,     ENDHI)
+SND_DMA_REG_RD8_WR8(endmi,     ENDMI)
+SND_DMA_REG_RD8_WR8(endlo,     ENDLO)
 
 static inline struct snd_dma_ctrl snd_dma_rd_ctrl()
 {
@@ -113,28 +113,28 @@ static inline void snd_dma_wr_ctrl(struct snd_dma_ctrl ctrl)
 
 #define snd_dma_wrs_ctrl(...) snd_dma_wr_ctrl((struct snd_dma_ctrl) __VA_ARGS__)
 
-static inline uint32_t snd_dma_rd32_start()
+static inline uint32_t snd_dma_rd32_base()
 {
-	return (snd_dma_rd8_starthi() << 16) |
-	       (snd_dma_rd8_startmi() <<  8) |
-	        snd_dma_rd8_startlo();
+	return (snd_dma_rd8_basehi() << 16) |
+	       (snd_dma_rd8_basemi() <<  8) |
+	        snd_dma_rd8_baselo();
 }
 
-static inline void *snd_dma_rd_start()
+static inline void *snd_dma_rd_base()
 {
-	return (void *)snd_dma_rd32_start();
+	return (void *)snd_dma_rd32_base();
 }
 
-static inline void snd_dma_wr32_start(uint32_t addr)
+static inline void snd_dma_wr32_base(uint32_t addr)
 {
-	snd_dma_wr8_starthi(addr >> 16);
-	snd_dma_wr8_startmi(addr >>  8);
-	snd_dma_wr8_startlo(addr);
+	snd_dma_wr8_basehi(addr >> 16);
+	snd_dma_wr8_basemi(addr >>  8);
+	snd_dma_wr8_baselo(addr);
 }
 
-static inline void snd_dma_wr_start(const void *buffer)
+static inline void snd_dma_wr_base(const void *buffer)
 {
-	snd_dma_wr32_start((uint32_t)buffer);
+	snd_dma_wr32_base((uint32_t)buffer);
 }
 
 static inline uint32_t snd_dma_rd32_count()
