cecff369c2ea17601a064d61ca2428d785755660
Fredrik Noring <noring@nocrew.org>
2026-03-10 18:28:43 +0100

    toslink: Set DRI symbol types global and defined
    Reference: Modern Atari System Software: A Programmer's Guide, 1993,
    HiSoft, chapter DRI, section Relocatable Format, pp. 180-181.

--- tool/symbol.c
+++ tool/symbol.c
@@ -12,11 +12,20 @@
 #include "tool/section.h"
 #include "tool/symbol.h"
 
+/*
+ * Reference: Modern Atari System Software: A Programmer's Guide, 1993,
+ * HiSoft, chapter DRI, section Relocatable Format, pp. 180-181.
+ */
 enum {
-	dri_symbol_type_bss  = 0x0100,
-	dri_symbol_type_text = 0x0200,
-	dri_symbol_type_data = 0x0400,
-	gst_symbol_type      = 0x0048,
+	dri_symbol_type_bss       = 0x0100,
+	dri_symbol_type_text      = 0x0200,
+	dri_symbol_type_data      = 0x0400,
+	dri_symbol_type_external  = 0x0800,
+	dri_symbol_type_register  = 0x1000,
+	dri_symbol_type_global    = 0x2000,
+	dri_symbol_type_equated   = 0x4000,
+	dri_symbol_type_defined   = 0x8000,
+	dri_symbol_type_extension = 0x0048,
 };
 
 struct dri_symbol {
@@ -25,14 +34,14 @@ struct dri_symbol {
 	uint32_t offset;
 } BE_STORAGE __attribute__((__packed__));
 
-struct gst_symbol {
+struct ext_symbol {
 	char name1[8];
 	uint16_t type;
 	uint32_t offset;
 	char name2[14];
 } BE_STORAGE __attribute__((__packed__));
 
-static bool gst_symbol(Elf_Sym *sym, Elf_Shdr *shdr, Elf_Ehdr *ehdr)
+static bool ext_symbol(Elf_Sym *sym, Elf_Shdr *shdr, Elf_Ehdr *ehdr)
 {
 	return strlen(elf_symbol(sym, shdr, ehdr)) >
 	       sizeof_member(struct dri_symbol, name);
@@ -45,9 +54,12 @@ static uint16_t dri_symbol_type(Elf_Sym *sym, Elf_Ehdr *ehdr)
 
 	Elf_Shdr *shdr = &elf_first_section(ehdr)[sym->st_shndx];
 
-	return text_section(shdr, ehdr) ? dri_symbol_type_text :
-	       data_section(shdr, ehdr) ? dri_symbol_type_data :
-	        bss_section(shdr, ehdr) ? dri_symbol_type_bss  : 0;
+	const uint16_t t = dri_symbol_type_global
+			 | dri_symbol_type_defined;
+
+	return text_section(shdr, ehdr) ? t | dri_symbol_type_text :
+	       data_section(shdr, ehdr) ? t | dri_symbol_type_data :
+	        bss_section(shdr, ehdr) ? t | dri_symbol_type_bss  : 0;
 }
 
 uint32_t symbol_size(const struct file *f,
@@ -60,8 +72,8 @@ uint32_t symbol_size(const struct file *f,
 
 	elf_for_each_sym (sym, shdr, ehdr)
 		if (dri_symbol_type(sym, ehdr))
-			size += gst_symbol(sym, shdr, ehdr) ?
-				sizeof(struct gst_symbol) :
+			size += ext_symbol(sym, shdr, ehdr) ?
+				sizeof(struct ext_symbol) :
 				sizeof(struct dri_symbol);
 
 	return size;
@@ -70,7 +82,7 @@ uint32_t symbol_size(const struct file *f,
 void append_symbols(struct file *tf, struct file *ef)
 {
 	BUILD_BUG_ON(sizeof(struct dri_symbol) != 14);
-	BUILD_BUG_ON(sizeof(struct gst_symbol) != 28);
+	BUILD_BUG_ON(sizeof(struct ext_symbol) != 28);
 
 	Elf_Ehdr *ehdr = (Elf_Ehdr *)ef->data;
 	Elf_Shdr *shdr;
@@ -93,15 +105,15 @@ void append_symbols(struct file *tf, struct file *ef)
 			memcpy(dri.name, s, n);
 			file_append(tf, (const void *)&dri, sizeof(dri));
 		} else {
-			struct gst_symbol gst = {
-				.type = dri_type | gst_symbol_type,
+			struct ext_symbol ext = {
+				.type = dri_type | dri_symbol_type_extension,
 				.offset = sym->st_value
 			};
 
-			memcpy(gst.name1, s, sizeof(gst.name1));
-			memcpy(gst.name2, &s[sizeof(gst.name1)],
-				min(n - sizeof(gst.name1), sizeof(gst.name2)));
-			file_append(tf, (const void *)&gst, sizeof(gst));
+			memcpy(ext.name1, s, sizeof(ext.name1));
+			memcpy(ext.name2, &s[sizeof(ext.name1)],
+				min(n - sizeof(ext.name1), sizeof(ext.name2)));
+			file_append(tf, (const void *)&ext, sizeof(ext));
 		}
 	}
 }
